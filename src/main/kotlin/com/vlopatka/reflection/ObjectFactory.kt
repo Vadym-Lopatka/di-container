f84a36b5eb0c0a6fb61714007e99c738aececde5
vlopatka <vlopatka@overonix.com>
2020-09-17 13:33:02 +0300

    [02] - added explanation comment

--- src/main/kotlin/com/vlopatka/reflection/ObjectFactory.kt
+++ src/main/kotlin/com/vlopatka/reflection/ObjectFactory.kt
@@ -6,8 +6,18 @@ import com.vlopatka.service.security.SecurityService
 
 object ObjectFactory {
 
-    private val config =
-        YmlConfig("com.vlopatka", mapOf(SecurityService::class.java to OutdoorSecurityService::class.java))
+    private val config = YmlConfig(
+        packageToScan = "com.vlopatka",
+        /**
+         * I have several implementations of SecurityService
+         * and here I am configuring which particular
+         * implementation I want my DI container to inject under SecurityService interface
+         * @see com.vlopatka.service.security.SecurityService
+         */
+        interfaceToImplementationMap = mapOf(
+            SecurityService::class.java to OutdoorSecurityService::class.java
+        )
+    )
 
     fun <T> createObject(type: Class<T>): T {
         return if (type.isInterface) {
