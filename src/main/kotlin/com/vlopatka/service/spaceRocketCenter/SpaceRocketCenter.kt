204b958824b4447c50e86c3b43c95e9381437a76
vlopatka <vlopatka@overonix.com>
2020-09-24 18:41:21 +0300

    [05] - cleanup

--- src/main/kotlin/com/vlopatka/service/spaceRocketCenter/SpaceRocketCenter.kt
+++ src/main/kotlin/com/vlopatka/service/spaceRocketCenter/SpaceRocketCenter.kt
@@ -2,12 +2,12 @@ package com.vlopatka.service.spaceRocketCenter
 
 import com.vlopatka.annotation.Injection
 import com.vlopatka.domain.Rocket
-import com.vlopatka.engine.ObjectFactory
 import com.vlopatka.service.notifier.Notifier
 import com.vlopatka.service.security.SecurityService
 import kotlin.random.Random
 
 class SpaceRocketCenter {
+
     @Injection
     private lateinit var notifier: Notifier
 
@@ -19,12 +19,12 @@ class SpaceRocketCenter {
         securityService.safetyCheck()
 
         notifier.periodicNotifier(msg = "Start in:", counter = 10, pendingInMs = 250)
-        val result = doLaunch(rocket)
 
+        val result = launch(rocket)
         notifier.notify("The launch result is: ${result.toReadableMessage()}")
     }
 
-    private fun doLaunch(rocket: Rocket): Boolean {
+    private fun launch(rocket: Rocket): Boolean {
         println("Land off!")
         val launchSuccessfulness = Random.nextInt(0, 100)
         return isSuccessFul(launchSuccessfulness)
