2b2b54e861ecd34f1cd7e6a335ac323ae4127a79
vlopatka <vlopatka@overonix.com>
2020-09-26 11:35:04 +0300

    [07] - refactored InjectPropertyAnnotationObjectConfigurator.kt

--- src/main/kotlin/com/vlopatka/domain/notifier/ConsoleNotifier.kt
+++ src/main/kotlin/com/vlopatka/domain/notifier/ConsoleNotifier.kt
@@ -7,6 +7,7 @@ import com.vlopatka.engine.annotation.Singleton
 class ConsoleNotifier : Notifier {
 
     @InjectProperty
+//    @GetFromConfig
     private lateinit var greeting: String
 
     override fun notify(msg: String) {
--- src/main/kotlin/com/vlopatka/engine/objectConfigurator/impls/InjectPropertyAnnotationObjectConfigurator.kt
+++ src/main/kotlin/com/vlopatka/engine/objectConfigurator/impls/InjectPropertyAnnotationObjectConfigurator.kt
@@ -4,6 +4,7 @@ import com.vlopatka.engine.annotation.InjectProperty
 import com.vlopatka.engine.context.ApplicationContext
 import com.vlopatka.engine.helper.FieldHelper.setValueToObject
 import com.vlopatka.engine.objectConfigurator.ObjectConfigurator
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.hasAnnotation
 import kotlin.reflect.full.memberProperties
 
@@ -12,14 +13,15 @@ class InjectPropertyAnnotationObjectConfigurator : ObjectConfigurator {
     private val propertiesMap: Map<String, String> = getFileAsMap("application.properties")
 
     override fun configure(obj: Any, context: ApplicationContext) {
-        for (field in obj::class.memberProperties) {
-            if (field.hasAnnotation<InjectProperty>()) {
-                val theAnnotation = field.annotations.find { it is InjectProperty } as InjectProperty
-                val value = theAnnotation.value.takeIf { it.isNotEmpty() } ?: propertiesMap[field.name]
 
-                setValueToObject(obj, field, value)
-            }
-        }
+        obj::class.memberProperties
+            .filter { it.hasAnnotation<InjectProperty>() }
+            .forEach { setValueToObject(obj, it, fetchValue(it)) }
+    }
+
+    private fun fetchValue(field: KProperty1<out Any, *>): String? {
+        val theAnnotation = field.annotations.find { it is InjectProperty } as InjectProperty
+        return theAnnotation.value.takeIf { it.isNotEmpty() } ?: propertiesMap[field.name]
     }
 
     private fun getFileAsMap(filename: String): Map<String, String> {
